78a82d38da22b5c34efe83b439771f661ab8a904
makserwhyy <flowtermo@gmail.com>
2025-12-25 16:51:53 +1000

    Update SearchMusic.py

--- SearchMusic.py
+++ SearchMusic.py
@@ -1,26 +1,23 @@
 import os
-from pyrogram import Client
 from pyrogram.enums import ParseMode
 
 async def sm_cmd(client, message, args):
     if not args:
-        await message.edit("<b>Напиши название песни</b>")
-        return
+        return await message.edit("<b>Напиши название песни</b>")
 
     query = " ".join(args)
     try:
         await message.edit(f"<b>🔍 Ищу:</b> <code>{query}</code>")
         
-        results = await client.get_inline_bot_results("lybot", query)
+        res = await client.get_inline_bot_results("lybot", query)
         
-        if not results or not results.results:
-            await message.edit(f"<b>❌ Не нашел <code>{query}</code></b>")
-            return
+        if not res or not res.results:
+            return await message.edit(f"<b>❌ Не нашел <code>{query}</code></b>")
 
         await client.send_inline_bot_result(
             chat_id=message.chat.id,
-            query_id=results.query_id,
-            result_id=results.results[0].id,
+            query_id=res.query_id,
+            result_id=res.results[0].id,
             reply_to_message_id=message.reply_to_message.id if message.reply_to_message else None
         )
         
